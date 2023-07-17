84a568386810f23be2323b924d2b5c423ef18883
James Fogarty <jayfo@users.noreply.github.com>
2023-07-17 11:48:04 -0700

    Code Format

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Ensure build and dev always assume a static export.
-    output: 'export',
+  // Ensure build and dev always assume a static export.
+  output: "export",
 
-    // Build output to dist directory.
-    distDir: 'dist',
-}
+  // Build output to dist directory.
+  distDir: "dist",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
