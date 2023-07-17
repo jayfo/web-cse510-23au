22f689aa604c9ded83f557b2a028db90472e052e
James Fogarty <jayfo@users.noreply.github.com>
2023-07-17 10:26:10 -0700

    Configure Build Output

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // Ensure build and dev always assume a static export.
+    output: 'export',
+
+    // Build output to dist directory.
+    distDir: 'dist',
+}
 
 module.exports = nextConfig
