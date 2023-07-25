23472d2999f1391dd62a71d0363aa2e03844a1ee
James Fogarty <jayfo@users.noreply.github.com>
2023-07-24 23:22:43 -0700

    Initial Simple Theme

--- src/components/ThemeRegistry/theme.ts
+++ src/components/ThemeRegistry/theme.ts
@@ -5,22 +5,18 @@
 import { createTheme } from "@mui/material/styles";
 
 const defaultTheme = createTheme({
+  // A purple and green palette from the DUB branding.
   palette: {
     mode: "light",
+    primary: {
+      main: "#4b2e83",
+    },
+    secondary: {
+      main: "#4cdc31",
+    },
   },
   typography: {
     fontFamily: "var(--font-roboto)",
-  },
-  components: {
-    MuiAlert: {
-      styleOverrides: {
-        root: ({ ownerState }) => ({
-          ...(ownerState.severity === "info" && {
-            backgroundColor: "#60a5fa",
-          }),
-        }),
-      },
-    },
   },
 });
 
