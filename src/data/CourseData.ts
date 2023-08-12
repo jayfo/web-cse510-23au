e3afc965ae56617e1a2343b4fd85cd69c9ce3c9c
James Fogarty <jayfo@users.noreply.github.com>
2023-08-12 11:56:35 -0700

    Define CourseData

---
+++ src/data/CourseData.ts
@@ -0,0 +1,9 @@
+import { CourseStoreData } from "@/types/CourseStore";
+
+const courseData: CourseStoreData = {
+  linkCanvas: "https://canvas.uw.edu/courses/1665830",
+  linkGitHub: undefined,
+  linkUniversitySyllabusGuidelines: undefined,
+};
+
+export default courseData;
--- src/stores/AppStoreInitialData.ts
+++ src/stores/AppStoreInitialData.ts
@@ -1,16 +1,11 @@
 import "server-only";
 
 import { AppStoreData } from "@/types/AppStore";
+import initialCourseStoreData from "@/data/CourseData"
 
 export function appStoreInitialData(): AppStoreData {
   return {
-    courseStoreData: {
-      linkCanvas: "",
-      linkGitHub: "",
-      linkUniversitySyllabusGuidelines: "",
-    },
-
-    testServerTime: new Date(),
+    courseStoreData: initialCourseStoreData,
   };
 }
 
