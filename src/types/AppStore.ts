b34821c08d6c32be223c36c8cd83b36c5d4df635
James Fogarty <jayfo@users.noreply.github.com>
2023-08-12 14:13:30 -0700

    Cleanup

--- src/types/AppStore.ts
+++ src/types/AppStore.ts
@@ -2,8 +2,6 @@ import CourseStore, { CourseStoreData } from "@/types/CourseStore";
 
 export interface AppStoreData {
   courseStoreData: CourseStoreData;
-
-  testServerTime: Date;
 }
 
 export interface AppStore extends AppStoreData {
