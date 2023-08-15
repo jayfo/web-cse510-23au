5b63fa7ba9fb1ff00ae1c8db6054fa34016f06fa
James Fogarty <jayfo@users.noreply.github.com>
2023-08-14 17:50:06 -0700

    Combination of NextJS and MUI Link

--- src/components/AppLink.tsx
+++ src/components/AppLink.tsx
@@ -1,7 +1,35 @@
 import * as React from "react";
 
 import MUILink from "@mui/material/Link";
-import NextJSLink from "next/link";
+import NextJSLink, { LinkProps as NextJSLinkProps } from "next/link";
+
+interface NextJSMUILinkProps extends NextJSLinkProps, React.PropsWithChildren {}
+export const NextJSMUILink = ({
+  children,
+  href,
+}: NextJSMUILinkProps): React.ReactElement => {
+  // Because MUILink internally provides the <a> element, we need passHref and legacyBehavior.
+  //
+  // As of 2023-08-14, this is described with the pages router:
+  // https://nextjs.org/docs/pages/api-reference/components/link
+  //
+  // As of 2023-08-14, this is not explicitly described with the app router:
+  // https://nextjs.org/docs/app/api-reference/components/link
+  //
+  // Without this, links were behaving strangely
+  // (e.g., not always honoring the hash component of a link).
+  //
+  // As of 2023-08-14, the NextJS <Link> prefetch functionality was problematic.
+  // Perhaps due to a negative interaction with the MUI <Link> hover behavior.
+  // Use Chrome to view a page, follow a <Link>, and then go back.
+  // Upon hovering over that same link, the app navigates to that link (i.e., without a click).
+  // Prefetch is therefore intentionally disabled here.
+  return (
+    <NextJSLink prefetch={false} href={href} passHref legacyBehavior>
+      <MUILink>{children}</MUILink>
+    </NextJSLink>
+  );
+};
 
 interface AppLinkProps extends React.PropsWithChildren<{}> {
   href: string;
@@ -44,19 +72,15 @@ export const AppLink = ({
   //
   // External links and file downloads are currently treated the same
   if (isExternalLink() || isFile()) {
-    // External links use normal MUI <Link>, are opened in "_blank".
+    // External links use a normal MUI <Link>, are opened in "_blank".
     return (
       <MUILink href={href} target="_blank" rel="noopener">
         {children}
       </MUILink>
     );
   } else {
-    // Internal links use NextJS <Link>, opened without a page load.
-    return (
-      <MUILink component={NextJSLink} href={href}>
-        {children}
-      </MUILink>
-    );
+    // Internal links use a NextJS <Link>, are opened without a page load.
+    return <NextJSMUILink href={href}>{children}</NextJSMUILink>;
   }
 };
 
