0f2a2168f57ddb11b19e403750f2009aba68ce48
James Fogarty <jayfo@users.noreply.github.com>
2023-08-15 19:09:59 -0700

    Document MDX Use of Raw HTML

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -12,11 +12,38 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     // Allows customizing built-in components.
     // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
+
+    // MDX links should all be our AppLink.
     a: ({ children, href }) => {
       assertNotNull(href);
 
       return <AppLink href={href}>{children}</AppLink>;
     },
+
+    // MDX is by default using raw html heading tags.
+    // This means MUI theming is not being applied to them.
+    // If we wanted to apply MUI theming, we would start with this.
+    // As is, the default MUI them heading font-sizes are much too large.
+    //
+    // h1: ({ children}) => {
+    //   return <Typography variant="h1">{children}</Typography>
+    // },
+    // h2: ({ children}) => {
+    //   return <Typography variant="h2">{children}</Typography>
+    // },
+    // h3: ({ children}) => {
+    //   return <Typography variant="h3">{children}</Typography>
+    // },
+    // h4: ({ children}) => {
+    //   return <Typography variant="h4">{children}</Typography>
+    // },
+    // h5: ({ children}) => {
+    //   return <Typography variant="h5">{children}</Typography>
+    // },
+    // h6: ({ children}) => {
+    //   return <Typography variant="h6">{children}</Typography>
+    // },
+
     ...components,
   };
 }
