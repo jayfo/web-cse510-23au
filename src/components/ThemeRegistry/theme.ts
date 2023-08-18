aff4ffecfd6df740c415c3abea27cb90e5f70314
James Fogarty <jayfo@users.noreply.github.com>
2023-08-18 07:49:01 -0700

    Apply HTML Defaults to Headings

--- src/components/ThemeRegistry/theme.ts
+++ src/components/ThemeRegistry/theme.ts
@@ -24,6 +24,47 @@ const theme = createTheme({
   },
   typography: {
     fontFamily: roboto.style.fontFamily,
+
+    // For internal consistency, apply default HTML heading typography.
+    // Format of any raw html heading elements will therefore be consistent.
+    //
+    // https://www.w3schools.com/cssref/css_default_values.php
+    h1: {
+      fontSize: "2em",
+      fontWeight: "bold",
+      marginBottom: "0.67em",
+      marginTop: "0.67em",
+    },
+    h2: {
+      fontSize: "1.5em",
+      fontWeight: "bold",
+      marginBottom: "0.83em",
+      marginTop: "0.83em",
+    },
+    h3: {
+      fontSize: "1.17em",
+      fontWeight: "bold",
+      marginBottom: "1.00em",
+      marginTop: "1.00em",
+    },
+    h4: {
+      fontSize: "1.0em",
+      fontWeight: "bold",
+      marginBottom: "1.33em",
+      marginTop: "1.33em",
+    },
+    h5: {
+      fontSize: "0.83em",
+      fontWeight: "bold",
+      marginBottom: "1.67em",
+      marginTop: "1.67em",
+    },
+    h6: {
+      fontSize: "0.67em",
+      fontWeight: "bold",
+      marginBottom: "2.33em",
+      marginTop: "2.33em",
+    },
   },
 });
 
