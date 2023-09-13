5a04a5113a6e8405dc8a8a8ece32763bd824dddc
James Fogarty <jayfo@users.noreply.github.com>
2023-09-13 06:22:23 -0700

    Initial SiteLink with outerComponent via AppLink

--- src/components/links/AppLink.tsx
+++ src/components/links/AppLink.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 
 import { NextJSMUILink } from "@/components/links/NextJSMuiLink";
+import { assertNotNullNotUndefined } from "@/types/Guards";
+import { assertIsOuterComponent, OuterComponent } from "@/types/OuterComponent";
 import { Link as MUILink } from "@mui/material";
+import Box from "@mui/material/Box";
 
 interface AppLinkProps extends React.PropsWithChildren<{}> {
   href: string;
   linkType?: "auto" | "internal" | "external" | "file";
+  outerComponent?: OuterComponent;
 }
 
 export const AppLink = ({
@@ -13,7 +17,16 @@ export const AppLink = ({
   href,
   // Default linkType to "auto".
   linkType = "auto",
+  // Default to no explicit outerComponent.
+  outerComponent = undefined,
 }: AppLinkProps): React.ReactElement => {
+  // Be sure there is a link.
+  assertNotNullNotUndefined(href);
+  // If provided an outerComponent, be sure it is valid.
+  if (outerComponent !== undefined) {
+    assertIsOuterComponent(outerComponent);
+  }
+
   // Determine whether this link is external to the app.
   // In that case, we should not use the app internal Link component.
   function isExternalLink(): boolean {
@@ -39,18 +52,26 @@ export const AppLink = ({
     );
   }
 
-  // Both types of link are rendered as <MUILink /> so they're consistently styled.
-  // An outer NextJSLink provides the desired behavior.
-  if (isExternalLink() || isFile()) {
-    // External links and file downloads are currently treated the same
-    // External links use a normal MUI <Link>, are opened in "_blank".
-    return (
-      <MUILink href={href} target="_blank" rel="noopener">
-        {children}
-      </MUILink>
-    );
+  const resultComponent: React.ReactElement = (() => {
+    // Both types of link are rendered as <MUILink /> so they're consistently styled.
+    // An outer NextJSLink provides the desired behavior.
+    if (isExternalLink() || isFile()) {
+      // External links and file downloads are currently treated the same
+      // External links use a normal MUI <Link>, are opened in "_blank".
+      return (
+        <MUILink href={href} target="_blank" rel="noopener">
+          {children}
+        </MUILink>
+      );
+    } else {
+      // Internal links use a NextJS <Link>, are opened without a page load.
+      return <NextJSMUILink href={href}>{children}</NextJSMUILink>;
+    }
+  })();
+
+  if (outerComponent) {
+    return <Box component={outerComponent}>{resultComponent}</Box>;
   } else {
-    // Internal links use a NextJS <Link>, are opened without a page load.
-    return <NextJSMUILink href={href}>{children}</NextJSMUILink>;
+    return resultComponent;
   }
 };
---
+++ src/components/links/SiteLink.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+
+import { AppLink } from "@/components/links/AppLink";
+import { assertNotNullNotUndefined } from "@/types/Guards";
+import { assertIsOuterComponent, OuterComponent } from "@/types/OuterComponent";
+import { SiteLinkKey } from "@/types/SiteLinks";
+
+interface SiteLinkProps extends React.PropsWithChildren<{}> {
+  linkKey: SiteLinkKey;
+  outerComponent?: OuterComponent;
+}
+
+export const SiteLink: React.FunctionComponent<SiteLinkProps> = ({
+  children,
+  linkKey,
+  // Default to no explicit outerComponent.
+  outerComponent = undefined,
+}) => {
+  // Be sure there is a link.
+  assertNotNullNotUndefined(linkKey);
+  assertNotNullNotUndefined(linkKey.href);
+  // If provided an outerComponent, be sure it is valid.
+  if (outerComponent !== undefined) {
+    assertIsOuterComponent(outerComponent);
+  }
+
+  const href = linkKey.href;
+
+  const resultComponentAnchor: React.ReactNode = (() => {
+    if (children) {
+      // If we have any anchor content, use that.
+      return children;
+    } else {
+      // If we do not have any anchor content, use an appropriate default.
+      return linkKey.anchor;
+    }
+  })();
+
+  return (
+    <AppLink href={href} outerComponent={outerComponent}>
+      {resultComponentAnchor}
+    </AppLink>
+  );
+};
