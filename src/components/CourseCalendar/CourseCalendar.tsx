4871dc170f312f2e2200a6d7f6a75b2cab44a742
James Fogarty <jayfo@users.noreply.github.com>
2023-09-21 06:13:30 -0700

    Move CourseCalendar

--- src/components/CourseCalendar/CourseCalendar.tsx
+++ src/components/CourseCalendar/CourseCalendar.tsx
@@ -325,3 +325,76 @@ export const CourseCalendar: React.FunctionComponent = () => {
     </Grid>
   );
 };
+
+// function renderAdditionalResources(calendarDateCurrent: CalendarDate) {
+//     if (calendarDateCurrent.additionalResources) {
+//         return (
+//             <Box>
+//                 <h3>Additional Optional Resources</h3>
+//                 <ul>
+//                     {calendarDateCurrent.additionalResources.map(readingCurrent => (
+//                         <li key={readingCurrent.title}>
+//                             <p><FormattedReading reading={readingCurrent}/></p>
+//                         </li>
+//                     ))}
+//                 </ul>
+//             </Box>
+//         )
+//     } else {
+//         return null;
+//     }
+// }
+//
+// function renderAwayJames(calendarDateCurrent: CalendarDate) {
+//     if (calendarDateCurrent.awayJames) {
+//         return (
+//             <Alert severity='info' sx={{marginBottom: 1}}>
+//                 James will be away.
+//             </Alert>
+//         );
+//     } else {
+//         return null;
+//     }
+// }
+//
+// function renderContent(calendarDateCurrent: CalendarDate) {
+//     if ('readingsStandard' in calendarDateCurrent) {
+//         return (
+//             <Box>
+//                 <p>Read the framing paper:</p>
+//                 <ul>
+//                     <li>
+//                         <p><FormattedReading reading={calendarDateCurrent.readingsStandard.framing}/></p>
+//                     </li>
+//                 </ul>
+//                 <p>Select one additional reading:</p>
+//                 <ul>
+//                     {calendarDateCurrent.readingsStandard.instances.map(readingCurrent => (
+//                         <li key={readingCurrent.title}>
+//                             <p><FormattedReading reading={readingCurrent}/></p>
+//                         </li>
+//                     ))}
+//                 </ul>
+//                 <h3>Standard Reading Format</h3>
+//                 <p>Post a reading report in the appropriate thread(s), by 11:59pm the night before class:</p>
+//                 <p><CourseInformationLink linkName={'linkDiscussion'}/></p>
+//             </Box>
+//         );
+//     } else if ('contentNonstandard' in calendarDateCurrent) {
+//         // Non-standard content rendered in an outside component
+//         return calendarDateCurrent.contentNonstandard;
+//     }
+// }
+//
+// function renderVirtual(calendarDateCurrent: CalendarDate) {
+//     if (calendarDateCurrent.virtual) {
+//         return (
+//             <Alert severity='info' sx={{marginBottom: 1}}>
+//                 Class will be conducted via Zoom, using meeting information provided by email and in Canvas.
+//             </Alert>
+//         );
+//     } else {
+//         return null;
+//     }
+// }
+//
