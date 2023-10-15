c2d84f1453dcf0302ccf1eb23ff5b0d0aaef23d1
James Fogarty <jayfo@users.noreply.github.com>
2023-10-15 08:48:21 -0700

    Code Format

--- src/data/CalendarData.tsx
+++ src/data/CalendarData.tsx
@@ -601,7 +601,7 @@ export const calendarData: {
             "Jacob O. Wobbrock, Krzysztof Z. Gajos, Shaun K. Kane, Gregg C. Vanderheiden",
           title: "Ability-Based Design",
           publicationText: "CACM, 2018",
-          link: 'https://canvas.uw.edu/files/109669327/'
+          link: "https://canvas.uw.edu/files/109669327/",
         },
         instances: [
           {
@@ -610,14 +610,14 @@ export const calendarData: {
             title:
               "Smart Touch: Improving Touch Accuracy for People with Motor Impairments with Template Matching",
             publicationText: "CHI 2016",
-            link: 'https://canvas.uw.edu/files/109669328/'
+            link: "https://canvas.uw.edu/files/109669328/",
           },
           {
             authorText: "Rachel L. Franz, Sasa Junuzovic, Martez Mott",
             title:
               "Nearmi: A Framework for Designing Point of Interest Techniques for VR Users with Limited Mobility",
             publicationText: "ASSETS 2021",
-            link: 'https://canvas.uw.edu/files/109669329/'
+            link: "https://canvas.uw.edu/files/109669329/",
           },
         ],
       },
